4caa8c49bff8fc09f8d40ac6d3419012b6750471
Alek <92643775+barealek@users.noreply.github.com>
2024-10-01 13:49:14 +0200

    Create webserver to serve app

---
+++ server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+type statusResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// WriteHeader captures the status code and calls the original WriteHeader
+func (w *statusResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		srw := &statusResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(srw, r)
+
+		var s string
+		statusCode := lipgloss.NewStyle().Background(lipgloss.Color("#007000")).Render(strconv.Itoa(srw.statusCode))
+		s += statusCode
+
+		fmt.Println(s)
+
+		// bufio.NewWriter(os.Stdout).WriteString(s)
+	})
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("dist"))
+	http.Handle("/", LogMiddleware(fs))
+	http.ListenAndServe(":3000", nil)
+}
