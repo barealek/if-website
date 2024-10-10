8c28cbffb7bbb81a1abaf17493cc5683402ec30d
Alek <92643775+barealek@users.noreply.github.com>
2024-10-10 18:01:59 +0200

    stof

--- server/server.go
+++ server/server.go
@@ -10,13 +10,13 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type statusResponseWriter struct {
+type statusInterceptor struct {
 	http.ResponseWriter
 	statusCode int
 }
 
 // WriteHeader captures the status code and calls the original WriteHeader
-func (w *statusResponseWriter) WriteHeader(code int) {
+func (w *statusInterceptor) WriteHeader(code int) {
 	w.statusCode = code
 	w.ResponseWriter.WriteHeader(code)
 }
@@ -25,37 +25,44 @@ func determineColorCode(code int) string {
 	var colorCode string = "#777777"
 
 	if strings.HasPrefix(strconv.Itoa(code), "2") {
-		colorCode = "#008000"
+		colorCode = "#7dfc3d"
 	}
 	if strings.HasPrefix(strconv.Itoa(code), "3") {
-		colorCode = "#000080"
+		colorCode = "#3897f5"
 	}
 	if strings.HasPrefix(strconv.Itoa(code), "4") {
-		colorCode = "#ffa500"
+		colorCode = "#ff9c45"
 	}
 	if strings.HasPrefix(strconv.Itoa(code), "5") {
-		colorCode = "#800000"
+		colorCode = "#ff4545"
 	}
 	return colorCode
 }
 
 func LogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		srw := &statusResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		srw := &statusInterceptor{ResponseWriter: w, statusCode: http.StatusOK}
 
 		next.ServeHTTP(srw, r)
 
+		var splitterStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#606060"))
+
+		// STATUS
 		var s string
 		colorCode := determineColorCode(srw.statusCode)
-		statusCode := lipgloss.NewStyle().Background(lipgloss.Color(colorCode)).Bold(true).Render(strconv.Itoa(srw.statusCode)) + " | "
+		statusCode := lipgloss.NewStyle().Foreground(lipgloss.Color(colorCode)).Bold(true).Render(strconv.Itoa(srw.statusCode)) + splitterStyle.Render(" | ")
 		s += statusCode
 
+		// IP
 		ip := r.Header.Get("CF-Connecting-IP")
+		if ip == "" {
+			ip = "127.0.0.1"
+		}
+		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#a0a0a0")).Render(ip) + splitterStyle.Render(" -> ")
 
-		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#a0a0a0")).Render(ip) + " -> "
-
+		// PATH
 		path := r.URL.Path
-		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#606060")).Render(path)
+		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#3a96dd")).Render(path)
 
 		fmt.Print(s + "\n")
 	})
