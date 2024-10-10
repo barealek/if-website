7c4b909efa0e498a32615453f9ab2c898b0cc574
Alek <92643775+barealek@users.noreply.github.com>
2024-10-10 17:41:26 +0200

    update logging

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 )
@@ -20,6 +21,24 @@ func (w *statusResponseWriter) WriteHeader(code int) {
 	w.ResponseWriter.WriteHeader(code)
 }
 
+func determineColorCode(code int) string {
+	var colorCode string = "#777777"
+
+	if strings.HasPrefix(strconv.Itoa(code), "2") {
+		colorCode = "#008000"
+	}
+	if strings.HasPrefix(strconv.Itoa(code), "3") {
+		colorCode = "#000080"
+	}
+	if strings.HasPrefix(strconv.Itoa(code), "4") {
+		colorCode = "#ffa500"
+	}
+	if strings.HasPrefix(strconv.Itoa(code), "5") {
+		colorCode = "#800000"
+	}
+	return colorCode
+}
+
 func LogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		srw := &statusResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
@@ -27,23 +46,24 @@ func LogMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(srw, r)
 
 		var s string
-		statusCode := lipgloss.NewStyle().Background(lipgloss.Color("#007000")).Render(strconv.Itoa(srw.statusCode)) + " - "
+		colorCode := determineColorCode(srw.statusCode)
+		statusCode := lipgloss.NewStyle().Background(lipgloss.Color(colorCode)).Bold(true).Render(strconv.Itoa(srw.statusCode)) + " | "
 		s += statusCode
 
 		ip := r.Header.Get("CF-Connecting-IP")
 
-		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#a0a0a0")).Render(ip) + " "
+		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#a0a0a0")).Render(ip) + " -> "
 
 		path := r.URL.Path
-		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#606060")).Render(path) + " "
+		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#606060")).Render(path)
 
 		fmt.Print(s + "\n")
 	})
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("dist"))
-	http.Handle("/", LogMiddleware(fs))
+    var fs = http.FileServer(http.Dir("dist"))
+    http.Handle("/", LogMiddleware(fs))
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
 	}
