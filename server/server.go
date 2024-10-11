b1426da78cc8a353c9f336e21ed13b8c0fb2bb27
Alek <92643775+barealek@users.noreply.github.com>
2024-10-11 20:28:25 +0200

    optimize memory

--- server/server.go
+++ server/server.go
@@ -40,29 +40,36 @@ func determineColorCode(code int) string {
 }
 
 func LogMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		srw := &statusInterceptor{ResponseWriter: w, statusCode: http.StatusOK}
 
+	var ipColorCode = "#a0a0a0"
+	var ipStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(ipColorCode))
+
+	var pathColorCode = "#3a96dd"
+	var pathStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(pathColorCode))
+
+	var splitterStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#606060"))
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get results of the true handler
+		srw := &statusInterceptor{ResponseWriter: w, statusCode: http.StatusOK}
 		next.ServeHTTP(srw, r)
 
-		var splitterStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#606060"))
 
 		// STATUS
 		var s string
 		colorCode := determineColorCode(srw.statusCode)
-		statusCode := lipgloss.NewStyle().Foreground(lipgloss.Color(colorCode)).Bold(true).Render(strconv.Itoa(srw.statusCode)) + splitterStyle.Render(" | ")
-		s += statusCode
+		s += lipgloss.NewStyle().Foreground(lipgloss.Color(colorCode)).Bold(true).Render(strconv.Itoa(srw.statusCode)) + splitterStyle.Render(" | ")
 
 		// IP
 		ip := r.Header.Get("CF-Connecting-IP")
 		if ip == "" {
 			ip = "127.0.0.1"
 		}
-		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#a0a0a0")).Render(ip) + splitterStyle.Render(" -> ")
+		s += ipStyle.Render(ip) + splitterStyle.Render(" -> ")
 
 		// PATH
 		path := r.URL.Path
-		s += lipgloss.NewStyle().Foreground(lipgloss.Color("#3a96dd")).Render(path)
+		s += pathStyle.Render(path)
 
 		fmt.Print(s + "\n")
 	})
