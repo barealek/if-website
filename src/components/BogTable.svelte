fbe66fcb18ed0985d55bada51b443aa3a5ec3f6f
Alek <92643775+barealek@users.noreply.github.com>
2024-10-11 18:44:47 +0200

    opdater sider

---
+++ src/components/BogTable.svelte
@@ -0,0 +1,14 @@
+<script>
+	import bog from "../data/bog.json"
+</script>
+
+<div class="flex items-center justify-center mt-8">
+	<div class="grid text-lg text-neutral-100">
+		{#each bog as entry, i}
+			<div class="flex gap-4 {i === 0 ? '' : 'border-t border-purple-500'} py-1 ">
+				<p class="font-bold">{entry[0]}</p>
+				<p class="text-right w-full">{entry[1]}</p>
+			</div>
+		{/each}
+	</div>
+</div>
