69bddad67abb192ca23c920e44afd44198494b6f
Alek <92643775+barealek@users.noreply.github.com>
2024-11-14 11:08:03 +0100

    y

--- php/index.php
+++ php/index.php
@@ -10,7 +10,7 @@
 <body>
    <div id="navbar"></div>
 
-   <section class="flex flex-col justify-center items-center h-[75vh] w-screen">
+   <section class="flex flex-col justify-center mt-12 items-center min-h-[75vh] w-screen">
 
       <img src="/img/alek.jpg" alt="billed af mig" class="object-cover rounded-full aspect-square w-72 ring-2 ring-offset-1 ring-blue-200">
       <?php
@@ -28,7 +28,7 @@
       <div class="flex items-center justify-center mt-8">
           <div class="grid text-lg text-neutral-900 bg-neutral-100">
               <?php foreach ($bog as $i => $entry): ?>
-                  <div class="flex gap-4 <?= $i === 0 ? '' : 'border-t border-purple-500' ?> py-1">
+                  <div class="flex gap-4 <?= $i === 0 ? '' : 'border-t border-blue-300' ?> py-1">
                       <p class="font-bold"><?= htmlspecialchars($entry[0]) ?></p>
                       <p class="w-full text-right"><?= htmlspecialchars($entry[1]) ?></p>
                   </div>
