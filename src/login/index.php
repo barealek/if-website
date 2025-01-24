21863a2eea7d4ef67ab047ca01d7d7f597b01102
Alek <92643775+barealek@users.noreply.github.com>
2025-01-24 12:48:46 +0100

    add label to inputs

--- src/login/index.php
+++ src/login/index.php
@@ -12,8 +12,12 @@
   <div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
     <div class="w-full max-w-sm space-y-10">
       <form accept-charset="UTF-8" role="form" method="post" action="/login/callback3.php" class="flex flex-col gap-4">
-        Brugernavn <input type="text" name="user" placeholder="..."/>
-        Kode <input type="text" name="pass" placeholder="..."/>
+        <label>
+          Brugernavn <input type="text" name="user" placeholder="..."/>
+        </label>
+        <label>
+          Kode <input type="text" name="pass" placeholder="..."/>
+        </label>
         <input type="submit" value="login"/>
       </form>
 
